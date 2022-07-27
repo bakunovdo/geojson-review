0b8d47bba6d9e9b0d504457095dc3e746b7ad2eb
bakunov <bakunov.do@gmail.com>
2022-07-27 21:08:54 +0300

    add tailwind

--- src/app/application.tsx
+++ src/app/application.tsx
@@ -1,7 +1,9 @@
+import "./styles.css";
+
 import type { Component } from "solid-js";
 
 const App: Component = () => {
-  return <div class="font-bold">Hello world</div>;
+  return <div class="font-bold text-lg">Hello world</div>;
 };
 
 export default App;
